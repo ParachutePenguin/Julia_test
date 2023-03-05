c8e51ccd0c97ec38cf4aed617c69274d77f09d03
ParachutePenguin <sst7092000@yahoo.co.jp>
2023-03-05 11:55:02 +0000

    day03

--- 7weeks_julia_day03.jl
+++ 7weeks_julia_day03.jl
@@ -56,9 +56,31 @@ begin
 	label="Leibniz", xlabel="cutoff num", ylabel="relative error")
 end
 
+# ╔═╡ 6db2c6af-cc24-436c-8035-de5dcea8b070
+# 区分求積法
+function kubun(N,x0,x1,f)
+	dx = (x1-x0)/N
+	a = 0.0
+	xn = range(x0, step=dx, length=N)
+	for x in xn
+		a += f(x)
+	end
+	return a*dx
+end
+
+# ╔═╡ 80daf127-4077-4385-978d-71218102c4d6
+begin
+	f(x) = 4/(1+x^2)
+	N = 10000
+	p = kubun(N,0,1,f)
+	println(p)
+end
+
 # ╔═╡ Cell order:
 # ╠═233e440a-7444-47bf-a389-bd234c710306
 # ╠═cdcbbd3e-bb1f-11ed-3955-c34a1a47031e
 # ╠═a6070107-90ef-4cc0-86bd-3ef0f113d887
 # ╠═2b302cec-6f79-49fe-8bca-61e1894dcd42
 # ╠═c77c0a5b-7951-4d57-81d7-467766ebb895
+# ╠═6db2c6af-cc24-436c-8035-de5dcea8b070
+# ╠═80daf127-4077-4385-978d-71218102c4d6
